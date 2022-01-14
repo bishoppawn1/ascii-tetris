e469d8dbec0eb4cfcd89f8028d93e72a4fc36762
Bishop Hall <bishop.e.hall@gmail.com>
2022-01-14 11:54:54 -0500

    helloworld

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,9 @@
-print('hello world')
+import os,time
+
+i = 0
+
+while True:
+    print('number of seconds' + str(i))
+    time.sleep(1)
+    i += 1
+    os.system('cls' if os.name == 'nt' else 'clear')
