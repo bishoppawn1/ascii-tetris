cf0dcfea98818922f1679ef25f0c8d5869ddbe64
Bishop Hall <bishop.e.hall@gmail.com>
2022-01-21 18:21:10 -0500

    draw

--- draw.py
+++ draw.py
@@ -1,3 +1,5 @@
+import time
+
 tetris_board_chars_map = {
     0: "█",
     1: "▓",
@@ -8,8 +10,9 @@ tetris_board_chars_map = {
 
 # converts one integer to ascii character
 def int_to_ascii(i):
-
     return tetris_board_chars_map[i]
+
+
 # convert int array to ascii characters
 
 def int_array_to_ascii_characters(int_array):
@@ -23,4 +26,14 @@ def draw_int_array(int_array):
     print(int_array_to_ascii_characters(int_array))
 
 
-draw_int_array([1, 0, 2, 1])
+def int_2d_array_to_ascii_characters(int_2d_array):
+    g = ""
+    for index in range(len(int_2d_array)):
+        g = g + int_array_to_ascii_characters(int_2d_array[index]) + "\n"
+    return g
+
+def draw_int_2d_array(int_2d_array):
+    print(int_2d_array_to_ascii_characters(int_2d_array))
+
+draw_int_2d_array([[0, 1, 3, 0], [2, 1, 3, 2], [1, 2, 1, 3]])
+
