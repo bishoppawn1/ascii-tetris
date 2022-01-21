06eaa3ca6363e5fc10f8f64d0bd3210b416edf15
Bishop Hall <bishop.e.hall@gmail.com>
2022-01-21 13:42:12 -0500

    ssss

---
+++ draw.py
@@ -0,0 +1,32 @@
+
+
+
+# converts one integer to ascii character
+def int_to_ascii(i):
+
+    if (i == 0):
+        return "█"
+    if (i == 1):
+        return "▓"
+    if (i == 2):
+        return "▒"
+    if (i == 3):
+        return "░"
+    else:
+        return
+
+
+# convert int array to ascii characters
+
+def int_array_to_ascii_characters(int_array):
+    g = ""
+    for index in range(len(int_array)):
+        g += int_to_ascii(int_array[index])
+    return g
+
+
+def draw_int_array(int_array):
+    print(int_array_to_ascii_characters(int_array))
+
+
+draw_int_array([1, 0, 2, 1])
