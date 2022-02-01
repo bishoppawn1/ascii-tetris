74fa5659c36226f8f7a9cb5c38ccfefe9d24145a
Bishop Hall <bishop.e.hall@gmail.com>
2022-01-31 23:19:49 -0500

    board

--- board.py
+++ board.py
@@ -17,20 +17,37 @@ classic_tetris_board = \
      [31, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 35],
      [34, 33, 33, 33, 33, 33, 33, 33, 33, 33, 33, 34]]
 
+
+# private functions
+def __flip_y(board, y):
+  """flips y value"""
+  return len(board) - 1 - y
+
+
+
+
+
+# public functions
+
+
+
+
 def value_at(board, x, y):
-    flipped_y = len(board) -1 -y
-    return board [flipped_y] [x]
+  """gets value"""
+  return board [__flip_y(board, y)] [x]
 
 def is_play_space(board, x, y):
-  coordanate_value = board [y] [x]
+  """checks is the space is a legit play space"""
+  coordanate_value = value_at(board, x, y)
   if coordanate_value == 30 | coordanate_value == 0:
     return True
   else:
     return False
 
 def is_occupied(board, x, y):
-  coordanate = board [y] [x]
-  if coordanate == 0 | coordanate == 31 | coordanate == 32 | coordanate == 33 | coordanate == 34 | coordanate == 35:
+  """ckecks if the space is occupied"""
+  coordanate_value = value_at(board, x, y)
+  if coordanate_value == 0 | coordanate_value == 31 | coordanate_value == 32 | coordanate_value == 33 | coordanate_value == 34 | coordanate_value == 35:
     return True
   else:
     return False
