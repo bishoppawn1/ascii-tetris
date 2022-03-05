2c0d1c54fd9063562665d51510b61e11da2843f9
Bishop Hall <bishop.e.hall@gmail.com>
2022-03-04 19:02:05 -0500

    tetrimino

--- Tetrimino.py
+++ Tetrimino.py
@@ -1,21 +1,34 @@
-class Tetrimino:
+class Tetromino:
     def __init__(self, orientation, location):
         self.orientation = orientation
         self.location = location
 
-    def can_rotate(self):
-        print(self.location)
+    def can_rotate(self, board):
         return True
 
-    def rotate(self):
+    def rotate(self, cw, ccw):
+
         return True
 
-    def can_move(self):
+    def can_move(self, board):
         return True
 
-    def move(self):
+    def move(self, board):
+        return True
+
+    def draw(self, board):
         return True
 
 
-tetrimino_1 = Tetrimino("up", "5, 5")
+
+
+class I_tetrimino(Tetromino):
+    def __init__(self, orientation, location):
+
+      super().__init__(orientation, location)
+
+
+
+
+tetrimino_1 = I_tetrimino("up", "5, 5")
 print(tetrimino_1.location)
