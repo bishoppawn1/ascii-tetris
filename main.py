23f8a25a1442130a47721ba7fc74fdf8854f2d94
Bishop Hall <bishop.e.hall@gmail.com>
2022-03-25 18:32:02 -0400

    input

--- main.py
+++ main.py
@@ -1,32 +1,36 @@
 import Tetromino
+import board
 import pieces.S_tetrimino
 import pieces.T_tetrimino
 import pieces.L_tetrimino
 import pieces.J_tetrimino
 import pieces.O_tetrimino
 import pieces.I_tetrimino
+import input
+import time
+
+board2 = board.copy(board.classic_tetris_board)
 
 
-tetrimino_1 = pieces.I_tetrimino.I_tetrimino(Tetromino.Orientation.RIGHT, [5, 7])
+def on_press2(key):
+    try:
+        print('Alphanumeric key pressed: {0} '.format(
+            key.char))
+    except AttributeError:
+        print('special key pressed: {0}'.format(
+            key))
+
+
+tetrimino_1 = pieces.S_tetrimino.S_Tetrimino(Tetromino.Orientation.UP, [6, 5])
 tetrimino_1.add_to_board(Tetromino.board.classic_tetris_board)
 
 
-tetrimino_2 = pieces.O_tetrimino.O_tetrimino(Tetromino.Orientation.LEFT, [2, 5])
-tetrimino_2.add_to_board(Tetromino.board.classic_tetris_board)
+def dummy_function():
+    tetrimino_1.rotate(Tetromino.Rotation.CCW)
+    tetrimino_1.add_to_board(board2)
+    Tetromino.draw.draw_int_2d_array(board2)
+    Tetromino.draw.draw_int_2d_array(Tetromino.board.classic_tetris_board)
 
 
-tetrimino_3 = pieces.J_tetrimino.J_tetrimino(Tetromino.Orientation.DOWN, [3, 11])
-tetrimino_3.add_to_board(Tetromino.board.classic_tetris_board)
-
-
-tetrimino_4 = pieces.L_tetrimino.L_tetrimino(Tetromino.Orientation.UP, [8, 5])
-tetrimino_4.add_to_board(Tetromino.board.classic_tetris_board)
-
-
-tetrimino_5 = pieces.T_tetrimino.T_tetromino(Tetromino.Orientation.UP, [3, 7])
-tetrimino_5.add_to_board(Tetromino.board.classic_tetris_board)
-
-
-tetrimino_6 = pieces.S_tetrimino.S_Tetrimino(Tetromino.Orientation.UP, [1, 5])
-tetrimino_6.add_to_board(Tetromino.board.classic_tetris_board)
-Tetromino.draw.draw_int_2d_array(Tetromino.board.classic_tetris_board)
+input.register_key_press_listener(on_press2)
+dummy_function()
